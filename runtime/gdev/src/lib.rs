5f41c4e6cd3617e5a4840f4dfe429acf41c05f42
librelois <c@elo.tf>
2022-01-31 01:11:21 +0100

    revert substrate PR #9732

--- runtime/gdev/src/lib.rs
+++ runtime/gdev/src/lib.rs
@@ -86,7 +86,6 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
-    state_version: 1,
 };
 
 /// The version information used to identify this runtime when compiled natively.
