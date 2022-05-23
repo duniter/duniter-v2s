74bcf3c227886970bd91952945a3bbfb309d5a14
librelois <c@elo.tf>
2022-05-23 22:39:34 +0200

    docker: upgrade image in live template

--- scripts/create-live-network.sh
+++ scripts/create-live-network.sh
@@ -22,7 +22,7 @@ SPEC_DIR="${4:-resources}"
 echo "CURRENCY=$CURRENCY"
 
 # constants
-DUNITER_IMAGE_TAG="sha-54010fa1"
+DUNITER_IMAGE_TAG="sha-68fd7b3a"
 
 # Clean and (re-)create working forders
 rm -rf $WORK_DIR
