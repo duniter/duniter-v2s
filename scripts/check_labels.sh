4415b0f5828198fe2d0577b8add29afa3852248c
Éloïs <c@elo.tf>
2022-06-19 15:55:06 +0200

    ci: MR with runtime changes should have a runtime-* milestone (!81)
    * ci: MR with runtime changes should have a runtime-* milestone

--- scripts/check_labels.sh
+++ scripts/check_labels.sh
@@ -6,12 +6,24 @@ RN_LABELS=(
   RN-silent
 )
 
+MR_LABELS=$1
+MR_MILESTONE=$2
+
+echo "Labels: $MR_LABELS"
+echo "Milestone: $MR_MILESTONE"
+
+if [[ "$MR_LABELS" == *"RN-runtime"* ]]; then
+  if [[ "$MR_MILESTONE" != "runtime-"* ]]; then
+    echo "MR with runtime changes should have a runtime-* milestone."
+    exit 1
+  fi
+fi
+
 for RN_LABEL in ${RN_LABELS[@]}; do
-  if [[ "$1" == *"$RN_LABEL"* ]]; then
-    echo "exit 0"
+  if [[ "$MR_LABELS" == *"$RN_LABEL"* ]]; then
     exit 0
   fi
 done
 
-echo "exit 1"
+echo "Every MR should have at least one RN-* label."
 exit 1
