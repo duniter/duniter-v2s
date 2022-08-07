213df4bcee021f427cb504063677a421193733e8
librelois <c@elo.tf>
2022-08-07 18:45:12 +0200

    use temporary release image to bootstrap gdev2

--- scripts/create-live-network.sh
+++ scripts/create-live-network.sh
@@ -22,7 +22,7 @@ SPEC_DIR="${4:-resources}"
 echo "CURRENCY=$CURRENCY"
 
 # constants
-DUNITER_IMAGE_TAG="debug-sha-ae65f879"
+DUNITER_IMAGE_TAG="sha-99fb985b"
 
 # Clean and (re-)create working forders
 rm -rf $WORK_DIR
