e585ab590a9b312971acc3566eca6dee7ccd6624
Pascal Engélibert <tuxmain@zettascript.org>
2022-09-08 14:23:23 +0200

    Fix homepage in Cargo.toml, project name in license (!106)
    * docs(all): fix homepage & name in license

--- pallets/oneshot-account/src/check_nonce.rs
+++ pallets/oneshot-account/src/check_nonce.rs
@@ -1,18 +1,18 @@
 // Copyright 2021 Axiom-Team
 //
-// This file is part of Substrate-Libre-Currency.
+// This file is part of Duniter-v2S.
 //
-// Substrate-Libre-Currency is free software: you can redistribute it and/or modify
+// Duniter-v2S is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, version 3 of the License.
 //
-// Substrate-Libre-Currency is distributed in the hope that it will be useful,
+// Duniter-v2S is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with Substrate-Libre-Currency. If not, see <https://www.gnu.org/licenses/>.
+// along with Duniter-v2S. If not, see <https://www.gnu.org/licenses/>.
 
 use crate::Config;
 
